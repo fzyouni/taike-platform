be4f8494be1ca9ea03a03b9f09f7993c1fcd33e4
fz <fzyouni@163.com>
2020-09-21 17:44:16 +0800

    值班班次代码提交

--- src/view/components/tables/tables.vue
+++ src/view/components/tables/tables.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
     <Card>
-      <tables ref="tables" editable searchable search-place="top" v-model="tableData" :columns="columns" @on-delete="handleDelete"/>
-      <Button style="margin: 10px 0;" type="primary" @click="exportExcel">导出为Csv文件</Button>
+      <Button style="margin: 10px 0;" type="primary" @click="addClassesInfo">新增</Button>
+      <Button style="margin: 10px;" type="primary" @click="exportExcel">导出</Button>
+      <Tables ref="tables" editable searchable search-place="top" v-model="tableData" :columns="columns"/>
+      <page :total="dataCount" :page-size="pageSize" @on-change="changePage" @on-page-size-change="nowPageSize"
+            show-total show-sizer show-elevator/>
     </Card>
   </div>
 </template>
 
 <script>
 import Tables from '_c/tables'
-import { getTableData } from '@/api/data'
+import { getDutyClassesList } from '@/api/dutyClasses'
 export default {
   name: 'tables_page',
   components: {
@@ -17,55 +20,51 @@ export default {
   },
   data () {
     return {
+      pageSize: 10,
+      dataCount: 0,
+      pageCurrent: 1,
       columns: [
-        { title: 'Name', key: 'name', sortable: true },
-        { title: 'Email', key: 'email', editable: true },
-        { title: 'Create-Time', key: 'createTime' },
-        {
-          title: 'Handle',
-          key: 'handle',
-          options: ['delete'],
-          button: [
-            (h, params, vm) => {
-              return h('Poptip', {
-                props: {
-                  confirm: true,
-                  title: '你确定要删除吗?'
-                },
-                on: {
-                  'on-ok': () => {
-                    vm.$emit('on-delete', params)
-                    vm.$emit('input', params.tableData.filter((item, index) => index !== params.row.initRowIndex))
-                  }
-                }
-              }, [
-                h('Button', '自定义删除')
-              ])
-            }
-          ]
-        }
+        { title: '班次名称', key: 'classesName' },
+        { title: '班次编码', key: 'classesCode' },
+        { title: '值班开始时间', key: 'classesStartTime' },
+        { title: '值班结束时间', key: 'classesEndTime' },
+        { title: '值班人数', key: 'classesPersonNumber' },
+        { title: '值班排序', key: 'classesOrder' }
       ],
-      tableData: []
+      tableData: [
+        { classesId: 5, classesName: '夜班', classesCode: 'classes5', classesStartTime: '2019-10-14 18:00:00', classesEndTime: '2019-10-15 09:00:00', classesPersonNumber: 1, classesDeleteFlag: '0', classesOrder: 4 }]
     }
   },
   methods: {
-    handleDelete (params) {
-      console.log(params)
+    addClassesInfo () {},
+    nowPageSize (index) {
+      // 实时获取当前需要显示的条数
+      this.pageSize = index
+    },
+    changePage () {
     },
     exportExcel () {
       this.$refs.tables.exportCsv({
         filename: `table-${(new Date()).valueOf()}.csv`
       })
+    },
+    initDutyClassesList () {
+      getDutyClassesList(this.pageCurrent, this.pageSize).then(res => {
+        debugger
+        this.dataCount = res.data.total
+        this.pageSize = res.data.pages
+        this.pageCurrent = res.data.current
+        this.tableData = res.data.records
+      }).catch(() => {
+        debugger
+        console.log('error')
+      })
     }
   },
   mounted () {
-    getTableData().then(res => {
-      this.tableData = res.data
-    })
+    this.initDutyClassesList()
   }
 }
 </script>
-
 <style>
-
 </style>
