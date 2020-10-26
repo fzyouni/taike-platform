63cea910b41378bd10d58a819a9824f61e3d76ef
fz <fzyouni@163.com>
2020-10-26 18:01:39 +0800

    增加数据字典节点api

---
+++ src/api/base/dataDict.js
@@ -0,0 +1,63 @@
+import axios from '@/libs/api.request'
+
+/**
+ *获取数据字典节点列表接口
+ * @param currentPage 当前页码
+ * @param pageSize 页容量
+ * @returns {*}
+ */
+export const getDictNodeList = (currentPage, pageSize) => {
+  return axios.request({
+    url: 'sysDict/getSysDictNodeList',
+    params: { currentPage, pageSize },
+    method: 'get'
+  })
+}
+/**
+ * 保存数据字典节点类别信息接口
+ * @param nodeCode 节点编码
+ * @param nodeName 节点名称
+ * @param isLeaf 叶子节点标志
+ * @param status 状态
+ * @param remark 描述
+ * @param nodeOrder 排序值
+ * @returns {*}
+ */
+export const addSysDictNodeInfo = (nodeCode, nodeName, isLeaf, status, remark, nodeOrder) => {
+  return axios.request({
+    url: 'sysDict/addSysDictNodeInfo',
+    data: { nodeCode, nodeName, isLeaf, status, remark, nodeOrder },
+    method: 'post'
+  })
+}
+/**
+ * 编辑指定的节点类别信息接口
+ * @param nodeId 节点类别ID
+ * @param nodeCode 节点编码
+ * @param nodeName 节点名称
+ * @param status 状态
+ * @param remark 描述
+ * @param nodeOrder 排序值
+ */
+export const updateSysDictNodeInfo = (nodeId, nodeCode, nodeName, status, remark, nodeOrder) => {
+  return axios.request({
+    url: 'sysDict/updateSysDictNodeInfo',
+    data: { nodeId, nodeCode, nodeName, status, remark, nodeOrder },
+    method: 'post'
+  })
+}
+
+/**
+ * 根据节点类型获取对应的字典值列表
+ * @param currentPage 当前页码
+ * @param pageSize 页容量
+ * @param nodeId 节点id
+ * @returns {*}
+ */
+export const showSysDictValueList = (currentPage, pageSize, nodeId) => {
+  return axios.request({
+    url: 'sysDictValue/showSysDictValueList',
+    params: { currentPage, pageSize, nodeId },
+    method: 'get'
+  })
+}
